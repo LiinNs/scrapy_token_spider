8166b2dcad84c565055999f021c56917cb2501e7
LiinNs <876056609@qq.com>
2018-12-29 16:14:35 +0800

    init project

---
+++ token_pipeline/settings.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for token_pipeline project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://doc.scrapy.org/en/latest/topics/settings.html
+#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+
+import random
+
+BOT_NAME = 'token_pipeline'
+
+SPIDER_MODULES = ['token_pipeline.spiders']
+NEWSPIDER_MODULE = 'token_pipeline.spiders'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'token_pipeline (+http://www.yourdomain.com)'
+USER_AGENT_LIST = [
+     "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+     "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+     "Opera/9.80 (Macintosh; Intel Mac OS X 10.14.1) Presto/2.12.388 Version/12.16",
+     "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16",
+     ]
+USER_AGENT =  random.choice(USER_AGENT_LIST)
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+CONCURRENT_REQUESTS = 8
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
+
+# Enable or disable spider middlewares
+# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'token_pipeline.middlewares.TokenPipelineSpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    'token_pipeline.middlewares.TokenPipelineDownloaderMiddleware': 543,
+#}
+# see https://github.com/cnu/scrapy-random-useragent
+# Scrapy Random User-Agent
+# DOWNLOADER_MIDDLEWARES = {
+#     'token_pipeline.middlewares.TokenPipelineDownloaderMiddleware': None,
+#     'random_useragent.RandomUserAgentMiddleware': 400
+# }
+
+# Enable or disable extensions
+# See https://doc.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+#ITEM_PIPELINES = {
+#    'token_pipeline.pipelines.TokenPipelinePipeline': 300,
+#}
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
---
+++ token_pipeline/spiders/quotes.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+
+    def start_requests(self):
+        urls = [
+            'https://info.binance.com/en/all'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse, meta={'proxy': 'http://127.0.0.1:1087'})
+
+    def parse(self, response):
+        for coin in response.css('div.s7v36e6-0.eJTsoN > div.name > span.fullName::text').extract():
+            detail_page = "https://info.binance.com/cn/currencies/%s" %  coin.replace(' ', '-')
+            yield scrapy.Request(detail_page, callback=self.parse_image, meta={'proxy': 'http://127.0.0.1:1087'})
+
+    def parse_image(self, response):
+        yield {
+            'coin': response.css('div.ix71fe-1.haNgdx > div.left-wapper > div.media-info > div.media-heading >div.instro::text').extract_first(),
+            'image_url': response.css('div.ix71fe-1.haNgdx > div.left-wapper > img::attr(src)').extract_first(),
+        }
